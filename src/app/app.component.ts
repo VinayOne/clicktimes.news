56e390a1707e6a6730d82ce0df6476361c929083
vinayone <vinaymunda@outlook.com>
2023-07-04 02:39:10 +0530

    gtm install

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, Inject, PLATFORM_ID } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 import { isPlatformBrowser } from '@angular/common';
 import { HomeService } from './modules/shared/home.service';
+import { NavigationEnd, Router } from '@angular/router';
+import { GoogleTagManagerService } from 'angular-google-tag-manager';
 
 @Component({
   selector: 'app-root',
@@ -12,22 +14,41 @@ export class AppComponent implements OnInit {
 
   static isBrowser = new BehaviorSubject<boolean>(false);
 
-  constructor(@Inject(PLATFORM_ID) private platformId: object, private homeService: HomeService) {
+  constructor(
+    @Inject(PLATFORM_ID) 
+    private platformId: object, 
+    private homeService: HomeService,
+    private gtmService: GoogleTagManagerService,
+    private router: Router
+    ) {
     AppComponent.isBrowser.next(isPlatformBrowser(platformId));
    }
 
   ngOnInit(): void {
-    // if (isPlatformBrowser(this.platformId)) {
-    //   const navMain = document.getElementById('navbarCollapse');
-    //   if (navMain) {
-    //     navMain.onclick = function onClick() {
-    //       if (navMain) {
-    //         navMain.classList.remove("show");
-    //       }
-    //     }
-    //   }
-    // }
+    if (isPlatformBrowser(this.platformId)) {
+      const navMain = document.getElementById('navbarCollapse');
+      if (navMain) {
+        navMain.onclick = function onClick() {
+          if (navMain) {
+            navMain.classList.remove("show");
+          }
+        }
+      }
+    }
   }
+
+
+  sendGtmEvents() {
+    this.router.events.forEach(item => {
+      if (item instanceof NavigationEnd) {
+          const gtmTag = {
+              event: 'page',
+              pageName: item.url
+          };
+          this.gtmService.pushTag(gtmTag);
+      }
+  });
+}
 
 
 }
